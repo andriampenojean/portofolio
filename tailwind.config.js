5232fd14472bd834b5eef99d9d1ce7e6b48a8c5b
Gwald321 <andriampenojean@gmail.com>
2025-02-12 17:08:32 +0300

    premier commit pour les configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // Recherche dans tous les fichiers source de React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
